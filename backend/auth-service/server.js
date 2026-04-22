f6ede2ca6fc2b67fc85d6a3e466397bfc153ab2d
Siddharth Bajaj <Bajajsiddharth10@gmail.com>
2026-04-22 20:05:05 +0530

    Create server.js

---
+++ backend/auth-service/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth.routes");
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/fsad_auth");
+
+app.use("/auth", authRoutes);
+
+app.listen(4000, () =>
+  console.log("Auth Service running on port 4000")
+);

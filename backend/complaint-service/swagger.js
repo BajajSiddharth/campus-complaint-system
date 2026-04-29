0a721800576e01e75b4319186d6390bf054e0fde
Siddharth Bajaj <Bajajsiddharth10@gmail.com>
2026-04-30 04:38:03 +0530

    Create swagger.js

---
+++ backend/complaint-service/swagger.js
@@ -0,0 +1,34 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Complaint Service API",
+      version: "1.0.0",
+      description: "API documentation for Campus Maintenance System"
+    },
+    servers: [
+      {
+        url: "http://localhost:5000"
+      }
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT"
+        }
+      }
+    },
+    security: [
+      {
+        bearerAuth: []
+      }
+    ]
+  },
+  apis: ["./routes/*.js"] // important
+};
+
+module.exports = swaggerJsdoc(options);

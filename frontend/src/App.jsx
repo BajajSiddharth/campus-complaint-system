69c86686324ba69595b8056ad3520b633bf30cb6
Siddharth Bajaj <Bajajsiddharth10@gmail.com>
2026-04-30 01:32:28 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import CreateComplaint from "./pages/CreateComplaint";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/create-complaint" element={<CreateComplaint />} />
     </Routes>
   );
 }

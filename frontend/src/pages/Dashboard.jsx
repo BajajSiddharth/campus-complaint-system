5dd6d6a8f5ff27979650a8625e94b582b1322ec1
Siddharth Bajaj <Bajajsiddharth10@gmail.com>
2026-04-29 23:52:44 +0530

    Create Dashboard.jsx

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function Dashboard() {
+  const [complaints, setComplaints] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/complaints", {
+        headers: {
+          Authorization: localStorage.getItem("token")
+        }
+      })
+      .then(res => setComplaints(res.data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Complaints</h2>
+      <ul>
+        {complaints.map(c => (
+          <li key={c._id}>
+            {c.title} - {c.status}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Dashboard;

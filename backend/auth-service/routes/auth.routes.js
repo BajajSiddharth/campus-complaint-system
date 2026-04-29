48e8918581c2490830f222bf1b0b8af8034e43f1
Siddharth Bajaj <Bajajsiddharth10@gmail.com>
2026-04-30 00:40:27 +0530

    Update auth.routes.js

--- backend/auth-service/routes/auth.routes.js
+++ backend/auth-service/routes/auth.routes.js
@@ -4,14 +4,25 @@ const User = require("../models/User");
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
-
+  
+  console.log("Login request:", email, password);
   const user = await User.findOne({ email, password });
+    
   if (!user) return res.status(401).json({ message: "Invalid credentials" });
 
   const token = jwt.sign(
     { userId: user._id, role: user.role },
     "SECRET_KEY"
   );
+
+  
+  return res.status(200).json({
+    token,
+    role: user.role,
+    userId: user._id,
+    name: user.name
+  });
+
 
   res.json({
     token,

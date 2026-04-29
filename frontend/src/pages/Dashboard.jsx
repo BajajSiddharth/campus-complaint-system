7dfc3191772aac0db9485e036a6deef069e584dd
Siddharth Bajaj <Bajajsiddharth10@gmail.com>
2026-04-30 02:45:06 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import NavBar from "../components/NavBar";
 
 function Dashboard() {
   const [complaints, setComplaints] = useState([]);
